b5763559ae7fd6ce60e3ef9065796c3b94dd8d11
VietKFC <vietngo1000@gmail.com>
2021-07-09 21:37:37 +0700

    splash ,bottom navigation

---
+++ app/src/main/java/com/example/vaccovid19/ui/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.vaccovid19.ui.home
+
+import com.example.vaccovid19.base.BaseFragment
+import com.example.vaccovid19.databinding.FragmentHomeLayoutBinding
+
+class HomeFragment: BaseFragment<FragmentHomeLayoutBinding>(FragmentHomeLayoutBinding::inflate) {
+    override fun initViews() {
+
+    }
+
+    override fun initData() {
+
+    }
+}
---
+++ app/src/main/java/com/example/vaccovid19/ui/splash/SplashActivity.kt
@@ -0,0 +1,16 @@
+@file:Suppress("DEPRECATION")
+
+package com.example.vaccovid19.ui.splash
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.vaccovid19.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(MainActivity.getIntent(this))
+        finish()
+    }
+}
